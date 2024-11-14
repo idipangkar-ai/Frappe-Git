8c204003911dba7484229aaaca74523f0f3c0698
Mr Groot <idipangkar02@gmail.com>
2024-11-14 13:13:30 +0530

    first commit

---
+++ github_apps/github_apps/doctype/enquire/test_enquire.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Dipangkar H and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEnquire(FrappeTestCase):
+	pass
